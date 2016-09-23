5b1afec7c410b05bbb95df1eb1abf1f221e3a974
dadiletta <dan.adiletta@gmail.com>
2016-09-23 12:55:29 -0400

    Initial main file

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from gopigo import *
 import time
 
+
 class Pigo(object):
     MIDPOINT = 90
 
     def __init__(self):
         print("Pigo online!")
-        fwd()
-        time.sleep(1)
-        stop()
+        servo(self.MIDPOINT)
+        if input("Am I looking straight ahead? (y/n): ") == "n":
+            while True:
+                response = input("Turn right, left, or am I done? (r/l/d): ")
+                if response == "r":
+                    self.MIDPOINT += 1
+                    servo(self.MIDPOINT)
+                elif response == "l":
+                    self.MIDPOINT -= 1
+                    servo(self.MIDPOINT)
+                else:
+                    print("Midpoint now saved to: " + self.MIDPOINT)
 
 
 variableName = Pigo()
