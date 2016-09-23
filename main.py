ec8b37de5df79602da9479e95f285ce6e9883aac
dadiletta <dan.adiletta@gmail.com>
2016-09-23 13:12:53 -0400

    Initial main file

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ MIDPOINT = 90
 
 def calibrate():
     print("Pigo online!")
-    servo(self.MIDPOINT)
+    servo(MIDPOINT)
     response = input("Am I looking straight ahead? (y/n): ")
     if response == 'n':
         while True:
             response = '"{}"'.format(input("Turn right, left, or am I done? (r/l/d): "))
             if response == "r":
-                self.MIDPOINT += 1
-                servo(self.MIDPOINT)
+                MIDPOINT += 1
+                servo(MIDPOINT)
             elif response == "l":
-                self.MIDPOINT -= 1
-                servo(self.MIDPOINT)
+                MIDPOINT -= 1
+                servo(MIDPOINT)
             else:
-                print("Midpoint now saved to: " + self.MIDPOINT)
+                print("Midpoint now saved to: " + MIDPOINT)
 
 
 calibrate()
