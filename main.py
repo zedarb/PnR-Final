2f7cceffb0bb52b9980857cfda4ee3900816116a
dadiletta <dan.adiletta@gmail.com>
2016-09-24 01:04:48 -0400

    Initial Pigo class

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from gopigo import *
 import time
 
 class Pigo(object):
-    MIDPOINT = 90
+    MIDPOINT = 77
 
     def __init__(self):
         print("Pigo online!")
         self.calibrate()
+        #let's use an event-driven model, make a handler of sorts to listen for "events"
+        while(True):
+            self.handler()
 
     def calibrate(self):
         print("Calibrating...")
@@ -17,10 +20,14 @@ class Pigo(object):
                 response = input("Turn right, left, or am I done? (r/l/d): ")
                 if response == "r":
                     self.MIDPOINT += 1
+                    print("Midpoint: " + str(self.MIDPOINT))
                     servo(self.MIDPOINT)
+                    time.sleep(.01)
                 elif response == "l":
                     self.MIDPOINT -= 1
+                    print("Midpoint: " + str(self.MIDPOINT))
                     servo(self.MIDPOINT)
+                    time.sleep(.01)
                 else:
                     print("Midpoint now saved to: " + str(self.MIDPOINT))
                     break
