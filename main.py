228e80c0de2f2d0c240c960e95218a5398f577e0
dadiletta <dan.adiletta@gmail.com>
2016-09-24 00:16:24 -0400

    Initial Pigo class

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from gopigo import *
 import time
 
-MIDPOINT = 90
+class Pigo(object):
+    MIDPOINT = 90
 
+    def __init__(self):
+        print("Pigo online!")
+        self.calibrate()
 
-def calibrate():
-    print("Pigo online!")
-    servo(MIDPOINT)
-    response = input("Am I looking straight ahead? (y/n): ")
-    if response == 'n':
-        while True:
-            response = '"{}"'.format(input("Turn right, left, or am I done? (r/l/d): "))
-            if response == "r":
-                MIDPOINT += 1
-                servo(MIDPOINT)
-            elif response == "l":
-                MIDPOINT -= 1
-                servo(MIDPOINT)
-            else:
-                print("Midpoint now saved to: " + MIDPOINT)
+    def calibrate():
+        print("Calibrating...")
+        servo(self.MIDPOINT)
+        response = input("Am I looking straight ahead? (y/n): ")
+        if response == 'n':
+            while True:
+                response = '"{}"'.format(input("Turn right, left, or am I done? (r/l/d): "))
+                if response == "r":
+                    self.MIDPOINT += 1
+                    servo(self.MIDPOINT)
+                elif response == "l":
+                    self.MIDPOINT -= 1
+                    servo(self.MIDPOINT)
+                else:
+                    print("Midpoint now saved to: " + self.MIDPOINT)
 
 
 calibrate()
