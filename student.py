9d486a129e7c4bdda8d7950b80c871e9cc1b3557
dadiletta <dan.adiletta@gmail.com>
2016-09-24 14:38:56 -0400

    Starting to move student work to a child class so I can continue to improve the base as the students' projects develop

--- student.py
+++ student.py
@@ -6,6 +6,14 @@ import pigo
 # from gopigo import *
 
 class GoPiggy(pigo.Pigo):
+    def __init__(self):
+        print("Piggy online!")
+        self.calibrate()
+        # let's use an event-driven model, make a handler of sorts to listen for "events"
+        while True:
+            self.stop()
+            self.handler()
+
     def nav(self):
         print("Piggy nav")
 
